2911147e764356ac0bba1724a7933b17b448a884
Jeffrey Huynh <jeffreyhuynh0@gmail.com>
2020-08-20 18:28:24 -0700

    Added styling and js to the carousel

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$('#myCarousel').on('slide.bs.carousel', function(e) {
+  if(e.relatedTarget.id == 'firstSlide'){
+		$(".carousel-inner").css('background-color', 'orange');
+  } else if(e.relatedTarget.id == 'secondSlide'){
+		$(".carousel-inner").css('background-color', 'green');
+  } else if(e.relatedTarget.id == 'thirdSlide') {
+		$(".carousel-inner").css('background-color', 'pink');
+	}
+})
